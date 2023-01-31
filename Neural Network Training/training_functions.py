f22b860d658d3e2e1edd0f0bd8dd1fe15fb529d2
Jeremy Carleton <jcarleton@tamu.edu>
2023-01-31 16:33:59 -0600

    Test a trained neural network on data with larger values

--- Neural Network Training/training_functions.py
+++ Neural Network Training/training_functions.py
@@ -7,9 +7,9 @@ def summarize_data(dataset):
     overshoot_cnt = undershoot_cnt = neither_cnt = 0
     num_entries = dataset.shape[0]
     for i in range(num_entries):
-        if dataset['Overshoot'][i] > 1e-5:
+        if dataset['Overshoot'][i] > 1e-10:
             overshoot_cnt = overshoot_cnt + 1
-        elif dataset['Undershoot'][i] > 1e-5:
+        elif dataset['Undershoot'][i] > 1e-10:
             undershoot_cnt = undershoot_cnt + 1
         else:
             neither_cnt = neither_cnt + 1
